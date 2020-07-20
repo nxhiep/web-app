475e7f66643880d65eb8b7a42ca1ee86afe80806
thanhtom26122000 <48215528+thanhtom26122000@users.noreply.github.com>
2020-07-20 11:56:27 +0700

    many change

--- src/index.tsx
+++ src/index.tsx
@@ -8,29 +8,30 @@ import App from './App.js';
 import configStore from './redux/store';
 import { register } from './serviceWorker';
 import { makeMainLoading } from './utils.js';
-
-// const styleNode = document.createComment('jss-insertion-point');
-// document.head.insertBefore(styleNode, document.head.firstChild);
+import { BrowserRouter } from 'react-router-dom';
 
 const jss = create({
-  ...jssPreset(),
-  insertionPoint: 'jss-insertion-point',
+    ...jssPreset(),
+    insertionPoint: 'jss-insertion-point',
 });
 
 const store = configStore();
-ReactDOM.render(
+ReactDOM.hydrate(
     <Provider store={store.store as any}>
         <PersistGate
             persistor={store.persistor}
             loading={makeMainLoading()}
         >
-        <StylesProvider jss={jss}>
-            <App />
-        </StylesProvider>
+            <StylesProvider jss={jss}>
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
+
+            </StylesProvider>
         </PersistGate>
     </Provider>,
-    document.getElementById('root') 
-  );
+    document.getElementById('root')
+);
 // if (module.hot) { module.hot.accept(App); }
 // serviceWorker.unregister();
 register();
